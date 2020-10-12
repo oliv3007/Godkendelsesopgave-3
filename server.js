a3b26e503eb85e4e8928ac14a6a1849c5a0e2081
oliv3007 <70469584+oliv3007@users.noreply.github.com>
2020-10-12 15:50:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+# Godkendelsesopgave-3
+
+var express = require('express')
+var app = express()
+
+app.listen(3000, () => console.log('Server is running on PORT 3000'))
+
+//Creating endpoints/route handlers
