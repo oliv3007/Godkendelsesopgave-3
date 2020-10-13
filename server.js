80b2c47aa8c4a6c024f6e40945212855df2ea042
oliv3007 <70469584+oliv3007@users.noreply.github.com>
2020-10-13 15:46:17 +0200

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-# Godkendelsesopgave-3
-
-var express = require('express')
-var app = express()
-
-app.listen(3000, () => console.log('Server is running on PORT 3000'))
-
-//Creating endpoints/route handlers
